249466b956e270652ccf7c6afb017844c6cb26c3
evabujazha <evabujazha@gmail.com>
2022-11-15 23:24:10 -0300

    TP3

---
+++ server.js
@@ -0,0 +1,26 @@
+const Container = require("./main");
+const products = new Container("products.txt");
+
+const express = require("express");
+const app = express();
+
+const server = app.listen(8080, () => {
+  console.log(`Servidor HTTP escuchando puerto ${server.address().port}`);
+});
+
+app.get("/productos", async (req, res) => {
+  const allProducts = await products.getAll();
+  res.send(allProducts);
+});
+app.get("/productoRandom", async (req, res) => {
+  const product = await products.getRandom();
+  res.send(product);
+});
+app.get("*", (req, res) => {
+  res.send(
+    '<h1 style="display:flex;justify-content:center;color:red;text-align:center">ruta mal escrita</h1>'
+  );
+});
+server.on("error", (error) => {
+  console.error(`Error en el server. ${error}`);
+});
